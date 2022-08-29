042221c285ff7e2328799b770d759096919c0ff8
igor vinicius <igor.santana@estudante.ifms.edu.br>
2022-08-29 10:53:16 -0400

    Adicionado Handlebars

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-
-
 const app = express();
-const port = 3000;
+const hbs = require('express-handlebars');
+const PORT = process.env.PORT || 3000;
+
+//configuracao do handleBars
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}));
 
 app.use('/', express.static('public'));
+app.set('view engine', 'hbs');
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`)
+//rota inicial
+//redenriza o home.hbs para abra dentro da tag {{{body}}} no layout
+app.get('/', (req, res)=>{
+    res.render('home');
+})
+
+app.listen(PORT, () => {
+    console.log("Servidor rodando em localhost:" + PORT)
 })

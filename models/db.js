e61b98b2cf84bb4c89b5b75eabcd7e06251c1245
igor <igor.santana@estudante.ifms.edu.br>
2022-09-19 14:40:51 -0400

    Adicionada conexão com o banco

---
+++ models/db.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('bd_loja', 'root', '',{
+
+host: '127.0.0.1',
+dialect: 'mysql',
+define: {
+charset: 'utf8',
+collate: 'utf8_general_ci',
+timestamps: true
+},
+
+loggin: false
+})
+
+sequelize.authenticate().then(function(){
+console.log('Conectado no banco com sucesso!')
+
+}).catch(function(err){
+console.log('Falha ao se conectar:'+err)
+
+})
